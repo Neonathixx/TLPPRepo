feb65554b263312c2e232dfa8f15c15acf5ef4e5
Neonathixx <neonatic21@gmail.com>
2026-05-24 23:39:12 +0800

    added signup.php

---
+++ signup.php
@@ -0,0 +1,26 @@
+<?php
+
+    include 'connection.php';
+
+    if(isset($_POST['submit'])){
+    $name = $_POST['Name'];
+    $username = $_POST['Username'];
+    $email = $_POST['Email'];
+    $password = password_hash($_POST['Password'], PASSWORD_DEFAULT);
+
+    $sql = $conn -> prepare("INSERT INTO Users(Name, Username, Email, Password) VALUES (?, ?, ?, ?)");
+
+    $sql->bind_param("sss", $name, $username, $email, $password);
+    $sql->execute();
+
+    if($sql->execute()){
+            echo "User registered successfully!";
+        } else {
+            echo "Error: " . $sql->error;
+        }
+
+    $ql->close();
+
+    }
+
+?>
